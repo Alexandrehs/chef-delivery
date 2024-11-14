90e5fce1733d3f06d421f684cc8c634ceeea3540
Alexandre Henrique da Silva <echo.alexandre@gmail.com>
2024-11-14 09:12:53 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Alexandre Henrique da Silva on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
